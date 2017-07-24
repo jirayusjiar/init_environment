98542e8b1c14c4a2a2e9cc7d72f42e28655a3db7
Jirayus Jiarpakdee <jiyoyo@hotmail.com>
2017-07-24 14:38:17 -0400

    include R script

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # init_environment
 Necessary files for a new working environment
 
+V0.2
+- Install R libraries script
+
 V0.1
 - Colorful bash
 - Git branch auto complete
---
+++ installLibraries.R
@@ -0,0 +1,32 @@
+list.of.packages <-
+    c(
+        "devtools",
+        "gtools",
+        "pROC",
+        "parallel",
+        "randomForest",
+        "compiler",
+        "car",
+        "Hmisc",
+        "stats",
+        "reshape2",
+        "moments",
+        "ROCR",
+        "effsize",
+        "gtools",
+        "foreach",
+        "doMC",
+        "plyr",
+        "caret",
+        "ScottKnottESD",
+        "rms",
+        "combinat"
+    )
+installedPackages <- installed.packages()[, "Package"]
+new.packages <-
+    list.of.packages[!(list.of.packages %in% installedPackages)]
+if (length(new.packages))
+    install.packages(new.packages, repos = "http://cran.rstudio.com/")
+if (!('DefectData' %in% installedPackages))
+    # if DefectData is not loaded
+    devtools::install_github("klainfo/DefectData")
